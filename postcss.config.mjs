6fd52cef56da40aca4e624c688dc57595bbe28f2
Gabriel <gabriel.m.ledesma96@gmail.com>
2025-06-13 12:44:30 -0300

    arreglo postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,4 @@
 const config = {
-    theme: {
-        extend: {
-            colors: {
-                primary: '#003432',
-                secondary: '#c3fb2c',
-                tertiary: '#c4fb2c63',
-            },
-        },
-    },
     plugins: {
         "@tailwindcss/postcss": {},
     },
